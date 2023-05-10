04d2878814235e25f88f9973e4ba3655d54f1d6f
Lupova19 <nadiqlupova@gmail.com>
2023-05-10 19:17:14 +0300

    first commit

---
+++ Cars/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+   internal class Car
+   {
+        private string brand;
+        private string model;
+        private string registratioinNumber;
+        private int yearOfManufacture;
+
+        private static int counter
+        {
+            get
+            {
+                return counter;
+            }
+        }
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public string RegistratioinNumber
+        {
+            get { return registratioinNumber; }
+            set { registratioinNumber = value; }
+        }
+        public int YearOfManufacture
+        {
+            get { return yearOfManufacture; }
+            set { yearOfManufacture = value; }
+            
+        }
+        public Car(string brand, string model, string registratioinNumber, int yearOfManufacture)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.RegistratioinNumber = registratioinNumber;
+            this.YearOfManufacture = yearOfManufacture;
+        }
+
+        public Car()
+        {
+            this.brand = null;
+            this.model = null;
+        }
+        public void Introduce()
+        {
+            Console.WriteLine($"Марка: {this.Brand}, Модел: {this.Model}, Рег. номер: {this.RegistratioinNumber}, " +
+            $"Година на производство: {this.YearOfManufacture}");
+        }
+        public void Move()
+        {
+            Console.WriteLine($"{this.Brand} {this.Model}: Brym-Brym-Brym");
+        }
+        public void Eco()
+        {
+            if (yearOfManufacture >= 2000)
+            {
+                Console.WriteLine("ECO");
+            }
+            else
+            {
+                Console.WriteLine("I'm not ECO car!");
+            }
+        }
+    }
+}
---
+++ Cars/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            Console.Write("Въведи брой коли: ");
+            int n = int.Parse(Console.ReadLine());
+      
+            for (int i = 0; i < n; i++)
+            {
+                Car car = new Car();
+                cars.Add(car);
+
+                Console.Write("Марка: ");
+                car.Brand = Console.ReadLine();
+                Console.Write("Модел: ");
+                car.Model = Console.ReadLine();
+                Console.Write("Рег. номер: ");
+                car.RegistratioinNumber = Console.ReadLine();
+                Console.Write("Година на производство: ");
+                car.YearOfManufacture = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Брой коли: {cars.Count}");
+
+                foreach (var item in cars)
+                {
+                    item.Introduce(); 
+                    item.Move(); 
+                    item.Eco();
+                } 
+            }
+        }
+    }
+}
